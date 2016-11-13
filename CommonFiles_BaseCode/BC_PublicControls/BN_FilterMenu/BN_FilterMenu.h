b700b42c95d83a8581906260bb7244a30a0a0911
newman <newman@newmantekiiMac.local>
2016-11-13 20:17:41 +0800

    修改筛选控件

--- CommonFiles_BaseCode/BC_PublicControls/BN_FilterMenu/BN_FilterMenu.h
+++ CommonFiles_BaseCode/BC_PublicControls/BN_FilterMenu/BN_FilterMenu.h
@@ -8,20 +8,56 @@
 
 #import <UIKit/UIKit.h>
 
+
+@interface BN_SubFilter : UIView
+
+- (void)getMenuDataRowArrayInBlock:(NSArray* (^)(NSInteger SupIndex, id SupData))block;
+- (void)getCellInBlock:(UITableViewCell* (^)(NSInteger SupIndex, NSIndexPath *indexPath, id SupData, id data))block;
+- (void)heightForRowInBlock:(CGFloat (^)(NSInteger SupIndex, NSIndexPath *indexPath, id SupData, id data))block;
+- (void)didDeselectRowAtIndexPathBlock:(void (^)(NSInteger SupIndex, NSIndexPath *indexPath, id SupData, id data))block;
+
+//刷新列表
+- (void)reloadData;
+
+- (void)setSupData:(id)supData supIndex:(NSInteger)supIndex;
+
+@end
+
+
 @interface BN_FilterMenu : UIView
 
 @property (nonatomic, assign) NSInteger fontSize;
 
 @property (nonatomic, strong) NSArray<NSString*>* menuArray;
+
 @property (nonatomic, strong) UIColor *textColor;
 @property (nonatomic, strong) UIColor *selectedTextColor;
-@property (nonatomic, strong) UIView *bottomView;
 
-- (void)getMenuDataArrayBlock:(NSArray* (^)(NSInteger index, NSString *title))block;
+@property (nonatomic, strong) BN_SubFilter *subFilterView;
+
+//放入section和row的数组
+- (void)getMenuDataRowArrayInBlock:(NSArray* (^)(NSInteger index, NSString *title, NSInteger section))block;
+- (void)getMenuDataSectionArrayInBlock:(NSArray* (^)(NSInteger index, NSString *title))block;
+- (void)getMenuBottomViewInBlock:(UIView* (^)(NSInteger index, NSString *title))block;
+
+//设置是否有子选项 默认为空
+- (void)haveSubFilterInBlock:(BOOL (^)(NSInteger index, NSString *title))block;
+
+//设置cell 默认为高度38的原声cell
 - (void)getCellInBlock:(UITableViewCell* (^)(NSInteger index, NSIndexPath *indexPath, id data))block;
 - (void)heightForRowInBlock:(CGFloat (^)(NSInteger index, NSIndexPath *indexPath, id data))block;
+
+//设置section 默认为空
+- (void)getSectionInBlock:(UIView* (^)(NSInteger index, NSInteger section, id data))block;
+- (void)heightForSectionInBlock:(CGFloat (^)(NSInteger index, NSInteger section, id data))block;
+
+//设置点击回调
 - (void)didDeselectRowAtIndexPathBlock:(void (^)(NSInteger index, NSIndexPath *indexPath, id data))block;
 
+//关闭列表
 - (void)closeMenu;
+//刷新列表
+- (void)reloadData;
 
 @end
+
--- CommonFiles_BaseCode/BC_PublicControls/BN_FilterMenu/BN_FilterMenu.m
+++ CommonFiles_BaseCode/BC_PublicControls/BN_FilterMenu/BN_FilterMenu.m
@@ -11,22 +11,161 @@
 #define BN_FilterSeparatorColor [UIColor colorWithRed:219/255.0 green:219/255.0 blue:219/255.0 alpha:1]
 #define kSeparatorColor [UIColor colorWithRed:219/255.0 green:219/255.0 blue:219/255.0 alpha:1]
 
-//- (void)getCellInBlock:(void (^)(NSInteger index, NSIndexPath *indexPath))block;
-//- (void)getSectionInBlock:(void (^)(NSInteger index, NSInteger section))block;
-//- (void)heightForHeaderInSectionBlock:(void (^)(NSInteger index, NSInteger section))block;
-//- (void)heightForRowInBlock:(void (^)(NSInteger index, NSIndexPath *indexPath))block;
+@interface BN_SubFilter()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSArray *dataArray;
+@property (nonatomic, strong) UIView *line;
+
+@property (nonatomic, strong)id supData;
+@property (nonatomic, assign)NSInteger supIndex;
+
+@property(nonatomic, copy) NSArray *(^rowDataArrayBlock)(NSInteger SupIndex, id SupData);
+
+@property(nonatomic, copy) UITableViewCell *(^cellBlock)(NSInteger SupIndex, NSIndexPath *indexPath, id SupData, id data);
+@property(nonatomic, copy) CGFloat (^heightForRowBlock)(NSInteger SupIndex, NSIndexPath *indexPath, id SupData, id data);
+
+@property(nonatomic, copy) void (^didDeselectRowBlock)(NSInteger SupIndex, NSIndexPath *indexPath, id SupData, id data);
+
+@end
+
+@implementation BN_SubFilter
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.tableView = [[UITableView alloc]initWithFrame:self.bounds];
+        self.tableView.dataSource = self;
+        self.tableView.delegate = self;
+        self.tableView.bounces = NO;
+        [self addSubview:self.tableView];
+        
+        self.line = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, 2)];
+        self.line.backgroundColor = ColorBtnYellow;
+        [self addSubview:self.line];
+        
+    }
+    return self;
+}
+
+- (void)setFrame:(CGRect)frame
+{
+    [super setFrame:frame];
+    self.tableView.frame = self.bounds;
+    self.line.frame = CGRectMake(0, 0, self.frame.size.width, 2);
+}
+
+- (void)reloadData
+{
+    [self.tableView reloadData];
+}
+
+- (void)getMenuDataRowArrayInBlock:(NSArray* (^)(NSInteger SupIndex, id SupData))block
+{
+    self.rowDataArrayBlock = block;
+}
+- (void)getCellInBlock:(UITableViewCell* (^)(NSInteger SupIndex, NSIndexPath *indexPath, id SupData, id data))block
+{
+    self.cellBlock = block;
+}
+- (void)heightForRowInBlock:(CGFloat (^)(NSInteger SupIndex, NSIndexPath *indexPath, id SupData, id data))block
+{
+    self.heightForRowBlock = block;
+}
+- (void)didDeselectRowAtIndexPathBlock:(void (^)(NSInteger SupIndex, NSIndexPath *indexPath, id SupData, id data))block
+{
+    self.didDeselectRowBlock = block;
+}
+
+- (void)setSupData:(id)supData supIndex:(NSInteger)supIndex
+{
+    _supData = supData;
+    _supIndex = supIndex;
+    self.dataArray = self.rowDataArrayBlock(supIndex,supData);
+    [self reloadData];
+}
+
+#pragma mark - tableview delegate and datasource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (UIView*)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    return [[UIView alloc]init];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    return nil;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 0;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+
+    CGFloat height = self.heightForRowBlock == nil ? 0 :self.heightForRowBlock(self.supIndex,indexPath,self.supData,self.dataArray[indexPath.row]);
+    return height?:38;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.dataArray.count;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *tableViewCell = self.cellBlock == nil ? nil:self.cellBlock(self.supIndex,indexPath,self.supData,self.dataArray[indexPath.row]);
+    
+    if (tableViewCell == nil) {
+        static NSString *TableSampleIdentifier = @"BN_FilterMenuTableViewCell";
+        tableViewCell = [tableView dequeueReusableCellWithIdentifier:TableSampleIdentifier];
+        if (tableViewCell == nil) {
+            tableViewCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:TableSampleIdentifier];
+        }
+        tableViewCell.textLabel.text = [NSString stringWithFormat:@"%@",self.dataArray[indexPath.row]];
+    }
+    return tableViewCell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    if (self.didDeselectRowBlock != nil) {
+        self.didDeselectRowBlock(self.supIndex,indexPath,self.supData,self.dataArray[indexPath.row]);
+    }
+}
+
+
+
+@end
 
 @interface BN_FilterMenu()<UITableViewDelegate,UITableViewDataSource>
 
 @property (nonatomic, strong) NSMutableArray *tempArray;
-@property (nonatomic, strong) NSArray *dataArray;
+@property (nonatomic, strong) NSArray *dataKeyArray;
+@property (nonatomic, strong) NSMutableDictionary *dataDic;
+
 @property (nonatomic, assign) int showIndex;
 @property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) UIView *backView;
 @property (nonatomic, strong) UIView *backGroundView;
+@property (nonatomic, strong) UIView *bottomView;
 
-@property(nonatomic, copy) NSArray *(^menuDataArrayBlock)(NSInteger index, NSString *title);
+@property(nonatomic, copy) NSArray *(^rowDataArrayBlock)(NSInteger index, NSString *title, NSInteger section);
+@property(nonatomic, copy) NSArray *(^sectionDataArrayBlock)(NSInteger index, NSString *title);
+@property(nonatomic, copy) UIView *(^bottomViewBlock)(NSInteger index, NSString *title);
+@property(nonatomic, copy) BOOL (^haveSubFilterBlock)(NSInteger index, NSString *title);
+
 @property(nonatomic, copy) UITableViewCell *(^cellBlock)(NSInteger index, NSIndexPath *indexPath, id data);
 @property(nonatomic, copy) CGFloat (^heightForRowBlock)(NSInteger index, NSIndexPath *indexPath, id data);
+
+@property(nonatomic, copy) UIView *(^sectionBlock)(NSInteger index, NSInteger section, id data);
+@property(nonatomic, copy) CGFloat (^heightForSectionBlock)(NSInteger index, NSInteger section, id data);
+
 @property(nonatomic, copy) void (^didDeselectRowBlock)(NSInteger index, NSIndexPath *indexPath, id data);
 
 @end
@@ -84,6 +223,13 @@
     self.tableView.showsHorizontalScrollIndicator = NO;
     self.tableView.bounces = NO;
     self.clipsToBounds = YES;
+    
+    self.backView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, 0)];
+    self.backView.clipsToBounds = YES;
+    [self.backView addSubview:self.tableView];
+    
+    self.subFilterView = [[BN_SubFilter alloc]init];
+    
     self.menuArray = @[@"please set menuArray"];
     
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(menuTapped:)];
@@ -100,20 +246,49 @@
     [self addGestureRecognizer:menuTapped];
 }
 
-- (void)getMenuDataArrayBlock:(NSArray* (^)(NSInteger index, NSString *title))block {
-    self.menuDataArrayBlock = block;
+- (void)getMenuDataRowArrayInBlock:(NSArray* (^)(NSInteger index, NSString *title, NSInteger section))block
+{
+    self.rowDataArrayBlock = block;
 }
 
-- (void)getCellInBlock:(UITableViewCell* (^)(NSInteger index, NSIndexPath *indexPath, id data))block {
+- (void)getMenuDataSectionArrayInBlock:(NSArray* (^)(NSInteger index, NSString *title))block
+{
+    self.sectionDataArrayBlock = block;
+}
+
+- (void)getCellInBlock:(UITableViewCell* (^)(NSInteger index, NSIndexPath *indexPath, id data))block
+{
     self.cellBlock = block;
 }
 
-- (void)heightForRowInBlock:(CGFloat (^)(NSInteger index, NSIndexPath *indexPath, id data))block {
+- (void)heightForRowInBlock:(CGFloat (^)(NSInteger index, NSIndexPath *indexPath, id data))block
+{
     self.heightForRowBlock = block;
 }
 
-- (void)didDeselectRowAtIndexPathBlock:(void (^)(NSInteger index, NSIndexPath *indexPath, id data))block {
+- (void)getSectionInBlock:(UIView* (^)(NSInteger index, NSInteger section, id data))block
+{
+    self.sectionBlock = block;
+}
+
+- (void)heightForSectionInBlock:(CGFloat (^)(NSInteger index, NSInteger section, id data))block
+{
+    self.heightForSectionBlock = block;
+}
+
+- (void)didDeselectRowAtIndexPathBlock:(void (^)(NSInteger index, NSIndexPath *indexPath, id data))block
+{
     self.didDeselectRowBlock = block;
+}
+
+- (void)getMenuBottomViewInBlock:(UIView* (^)(NSInteger index, NSString *title))block
+{
+    self.bottomViewBlock = block;
+}
+
+- (void)haveSubFilterInBlock:(BOOL (^)(NSInteger index, NSString *title))block
+{
+    self.haveSubFilterBlock = block;
 }
 
 - (void)closeMenu {
@@ -147,7 +322,11 @@
         self.showIndex = -1;
     } else {
         self.showIndex = (int)touchIndex;
-        self.dataArray = self.menuDataArrayBlock == nil ?@[@"no data"] :self.menuDataArrayBlock(self.showIndex,self.menuArray[self.showIndex])?:@[@"no data"];
+        self.dataKeyArray = self.sectionDataArrayBlock(self.showIndex,self.menuArray[self.showIndex]);
+        self.dataDic = [[NSMutableDictionary alloc]init];
+        [self.dataKeyArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            self.dataDic[obj] = [@[] mutableCopy];
+        }];
     }
     
     NSLog(@"%f",self.tableView.contentSize.height);
@@ -205,6 +384,15 @@
     CGPathRelease(bound);
     lineLayer.position = CGPointMake(self.frame.size.width/2.0, self.frame.size.height);
     [self.layer addSublayer:lineLayer];
+}
+
+- (void)reloadData
+{
+    self.dataDic = [[NSMutableDictionary alloc]init];
+    [self.dataKeyArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        self.dataDic[obj] = [@[] mutableCopy];
+    }];
+    [self.tableView reloadData];
 }
 
 #pragma mark - 绘图
@@ -303,21 +491,22 @@
 }
 
 - (void)animateTableView {
+    
     if (self.showIndex == -1) {
         [UIView animateWithDuration:0.25 animations:^{
-            self.tableView.frame = CGRectMake(self.frame.origin.x,
+            self.backView.frame = CGRectMake(self.frame.origin.x,
                                               self.frame.origin.y + self.frame.size.height,
                                               self.frame.size.width,
                                               0);
             self.backGroundView.alpha = 0;
         } completion:^(BOOL finished) {
-            [self.tableView removeFromSuperview];
+            [self.backView removeFromSuperview];
             [self.backGroundView removeFromSuperview];
         }];
         
     } else {
-        [self.tableView removeFromSuperview];
-        self.tableView.frame = CGRectMake(self.frame.origin.x,
+        [self.backView removeFromSuperview];
+        self.backView.frame = CGRectMake(self.frame.origin.x,
                                           self.frame.origin.y + self.frame.size.height,
                                           self.frame.size.width,
                                           0);
@@ -327,17 +516,48 @@
                                                self.superview.frame.size.height - self.frame.origin.y - self.frame.size.height);
 
         [self.superview addSubview:self.backGroundView];
-        [self.superview addSubview:self.tableView];
+        [self.superview addSubview:self.backView];
         [self.tableView reloadData];
+        
+        CGFloat maxHeight = self.superview.frame.size.height - self.frame.origin.y - self.frame.size.height - 35;
+        
+        [self.bottomView removeFromSuperview];
+        self.bottomView = self.bottomViewBlock(self.showIndex,self.menuArray[self.showIndex])?:[[UIView alloc]init];
+        self.tableView.frame = CGRectMake(0,
+                                          0,
+                                          self.frame.size.width,
+                                          MIN(self.tableView.contentSize.height, maxHeight - self.bottomView.frame.size.height));
+        self.bottomView.frame = CGRectMake(0,
+                                      self.tableView.frame.size.height,
+                                      self.frame.size.width,
+                                      self.bottomView.frame.size.height);
+        [self.backView addSubview:self.tableView];
+        [self.backView addSubview:self.bottomView];
+        [self.tableView setContentOffset:CGPointMake(0,0) animated:NO];
+        
+        //判断是否有子项目
+        if (self.haveSubFilterBlock != nil && self.haveSubFilterBlock(self.showIndex,self.menuArray[self.showIndex]))
+        {
+            self.subFilterView.frame = CGRectMake(self.frame.size.width/2.0,
+                                                  self.heightForSectionBlock(self.showIndex,0,self.dataKeyArray[0]),
+                                                  self.frame.size.width/2.0,
+                                                  self.tableView.frame.size.height - self.heightForSectionBlock(self.showIndex,0,self.dataKeyArray[0]));
+            self.subFilterView.supIndex = 0;
+            NSArray *array = [self getRowArrayInSection:0];
+            [self.backView addSubview:self.subFilterView];
+            [self.subFilterView setSupData:array.count > 0 ? array[0] : nil supIndex:0];
+        }
+        else
+        {
+            [self.subFilterView removeFromSuperview];
+        }
         
         [UIView animateWithDuration:0.25 animations:^{
             
-            CGFloat maxHeight = self.superview.frame.size.height - self.frame.origin.y - self.frame.size.height - 35;
-            
-            self.tableView.frame = CGRectMake(self.frame.origin.x,
+            self.backView.frame = CGRectMake(self.frame.origin.x,
                                               self.frame.origin.y + self.frame.size.height,
                                               self.frame.size.width,
-                                              MIN(self.tableView.contentSize.height, maxHeight));
+                                              MIN(self.tableView.contentSize.height + self.bottomView.frame.size.height, maxHeight));
             self.backGroundView.alpha = 0.25;
         } completion:^(BOOL finished) {
             ;
@@ -349,57 +569,77 @@
 
 #pragma mark - tableview delegate and datasource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return self.dataKeyArray.count;
 }
 
 - (UIView*)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
-    return [[UIView alloc]init];
+    UIView *view = self.sectionBlock == nil ? nil : self.sectionBlock(self.showIndex,section,self.dataKeyArray[section]);
+    return view?:[[UIView alloc]init];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
-    return 0.01;
+    CGFloat height = self.heightForSectionBlock == nil ? 0 :self.heightForSectionBlock(self.showIndex,section,self.dataKeyArray[section]);
+    return height;
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
-    return self.bottomView?:[[UIView alloc]init];
+    return nil;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
-    if (self.bottomView != nil) {
-        return self.bottomView.frame.size.height;
-    }
     return 0;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
-    CGFloat height = self.heightForRowBlock == nil ? 0 :self.heightForRowBlock(self.showIndex,indexPath,self.dataArray[indexPath.row]);
+    NSArray *array = [self getRowArrayInSection:indexPath.section];
+    CGFloat height = self.heightForRowBlock == nil ? 0 :self.heightForRowBlock(self.showIndex,indexPath,array[indexPath.row]);
     return height?:38;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.dataArray.count;
+    NSArray *array = [self getRowArrayInSection:section];
+    return array.count;
 }
 
 - (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    UITableViewCell *tableViewCell = self.cellBlock == nil ? nil:self.cellBlock(self.showIndex,indexPath,self.dataArray[indexPath.row]);
+    NSArray *array = [self getRowArrayInSection:indexPath.section];
+    UITableViewCell *tableViewCell = self.cellBlock == nil ? nil:self.cellBlock(self.showIndex,indexPath,array[indexPath.row]);
     
     if (tableViewCell == nil) {
         static NSString *TableSampleIdentifier = @"BN_FilterMenuTableViewCell";
         tableViewCell = [tableView dequeueReusableCellWithIdentifier:TableSampleIdentifier];
         if (tableViewCell == nil) {
             tableViewCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:TableSampleIdentifier];
+            tableViewCell.separatorInset = UIEdgeInsetsMake(0, 0, 0, tableViewCell.bounds.size.width);
+            tableViewCell.backgroundColor = [UIColor colorWithWhite:0.92 alpha:1];
         }
-        tableViewCell.textLabel.text = [NSString stringWithFormat:@"%@",self.dataArray[indexPath.row]];
+        tableViewCell.textLabel.text = [NSString stringWithFormat:@"%@",array[indexPath.row]];
     }
     return tableViewCell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    NSArray *array = [self getRowArrayInSection:indexPath.section];
     if (self.didDeselectRowBlock != nil) {
-        self.didDeselectRowBlock(self.showIndex,indexPath,self.dataArray[indexPath.row]);
+        self.didDeselectRowBlock(self.showIndex,indexPath,array[indexPath.row]);
     }
+    if (self.haveSubFilterBlock != nil && self.showIndex >= 0 && self.haveSubFilterBlock(self.showIndex,self.menuArray[self.showIndex]))
+    {
+        [self.subFilterView setSupData:array[indexPath.row] supIndex:indexPath.row];
+    }
+}
+
+- (NSArray *)getRowArrayInSection:(NSInteger)section
+{
+    NSArray *array = self.dataDic[self.dataKeyArray[section]];
+    if (array.count == 0)
+    {
+        array = self.rowDataArrayBlock(self.showIndex,self.menuArray[self.showIndex],section);
+        self.dataDic[self.dataKeyArray[section]] = array;
+    }
+    return array;
 }
 
 @end
